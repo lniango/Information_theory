2616dcea9124e2d9b81e840dd4747c3fad44328e
Louis Niango <louis.niango14@gmail.com>
2026-06-01 23:23:13 +0200

    Implementation of basic arithmetic coding algorithm to code sequence of symbols

---
+++ information_th.py
@@ -0,0 +1,26 @@
+# https://www.khanacademy.org/computing/computer-science/informationtheory/info-theory/v/intro-information-theory
+# Arithmetic coding: https://github.com/nayuki/Reference-arithmetic-coding/tree/master/cpp
+# https://web.stanford.edu/class/ee398a/projects.htm
+# https://ocw.mit.edu/courses/6-441-information-theory-spring-2016/pages/lecture-notes/
+# Github repo: https://github.com/leandromoreira/digital_video_introduction#vlc-coding
+# 
+
+import numpy as np 
+import matplotlib.pyplot as plt 
+import cv2 as cv
+
+# Custom arithmetic coding
+def arithmetic_coding(table, proba):
+    """
+    table  : table of symbols
+    proba. : likelihood of symbols 
+    return : bitstream
+    """
+    range_min = 0
+    range_max = 1
+    for t in table:
+        
+    
+    
+def aritmetic_decoding():
+    
